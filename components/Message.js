329bd8ca9d0f806b18da3fea243e1073f2501c62
emilybonar <emilykbonar@gmail.com>
2021-01-07 13:49:08 -0600

    Improved time printing

--- components/Message.js
+++ components/Message.js
@@ -1,11 +1,12 @@
 export default function Message(props) {
+	let time = props.time.toLocaleTimeString() + " " + props.time.toDateString();
 	return (
 		<div className="my-2 ">
 			<div className="p-2 border-2 border-black">{props.text}</div>
 			<div className="flex">
 				<p>{props.name}</p>
 				<p className="flex-1"></p>
-				<p>{props.time.toString()}</p>
+				<p>{time}</p>
 			</div>
 		</div>
 	);
