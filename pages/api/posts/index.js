bbdfe583151fe44d8ae065c9ec200cf76fccf789
emilybonar <emilykbonar@gmail.com>
2021-01-07 09:27:02 -0600

    Improved database

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -2,14 +2,11 @@
 import { prisma } from "../_base";
 
 export default async function handle(req, res) {
-	const posts = await prisma.post.findMany({ include: { author: true } });
-	let correctedPosts = posts.map((post) => {
-		post.author = post.author.name;
-		return post;
-	});
+	const posts = await prisma.post.findMany();
+	console.log(posts);
 	res.setHeader(
 		"Cache-Control",
 		"public, max-age=120, stale-while-revalidate=60",
 	);
-	res.json(correctedPosts);
+	res.json(posts);
 }
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,6 @@ export default function Home() {
 	fetch(`${server}/api/posts`) //happens very quickly, might need to rate limit somehow
 		.then((response) => response.json())
 		.then((data) => {
-			console.log(data);
 			setPosts(data);
 		});
 	return (
@@ -20,17 +19,12 @@ export default function Home() {
 			</Head>
 			<div className="w-4/5 m-auto">
 				<MessageInput />
-				<Message name="test" text="testing" />
-				<Message
-					name="test"
-					text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tincidunt augue ut mauris tincidunt, eu pharetra quam accumsan. Mauris tempor lobortis dapibus. Etiam at dapibus risus. Nullam consequat pharetra neque sit amet dignissim. Quisque fermentum lorem et tincidunt luctus. Etiam posuere volutpat enim ac condimentum. Duis ut ipsum finibus, condimentum ligula sit amet, rutrum lectus. Phasellus sed diam erat. Donec vestibulum quis diam sed condimentum. Nullam aliquam justo nunc, eu finibus turpis commodo vitae. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean vitae diam eget nibh tincidunt aliquam non sit amet orci. Maecenas elementum rutrum eleifend. Vivamus consequat fringilla placerat. Phasellus facilisis sit amet quam ac porta. "
-				/>
 				<Message
 					name="test"
 					text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tincidunt augue ut mauris tincidunt, eu pharetra quam accumsan. Mauris tempor lobortis dapibus. Etiam at dapibus risus. Nullam consequat pharetra neque sit amet dignissim. Quisque fermentum lorem et tincidunt luctus. Etiam posuere volutpat enim ac condimentum. Duis ut ipsum finibus, condimentum ligula sit amet, rutrum lectus. Phasellus sed diam erat. Donec vestibulum quis diam sed condimentum. Nullam aliquam justo nunc, eu finibus turpis commodo vitae. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean vitae diam eget nibh tincidunt aliquam non sit amet orci. Maecenas elementum rutrum eleifend. Vivamus consequat fringilla placerat. Phasellus facilisis sit amet quam ac porta. "
 				/>
 				{posts.map((post) => (
-					<Message name={post.author} text={post.content} />
+					<Message name={post.userName} text={post.content} />
 				))}
 			</div>
 		</div>
