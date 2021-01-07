61109b52366f78a84c5aa683e723ac663711c42f
emilybonar <emilykbonar@gmail.com>
2021-01-07 11:28:25 -0600

    Seperated username and time

--- components/Message.js
+++ components/Message.js
@@ -3,8 +3,9 @@ export default function Message(props) {
 	return (
 		<div className="my-2 ">
 			<div className="p-2 border-2 border-black">{props.text}</div>
-			<div className="flex gap-2">
+			<div className="flex">
 				<p>{props.name}</p>
+				<p className="flex-1"></p>
 				<p>{props.time.toString()}</p>
 			</div>
 		</div>
