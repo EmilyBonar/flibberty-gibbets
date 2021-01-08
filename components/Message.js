b6169a9a7c8e100601bf8b70c3c42acf047abf2c
emilybonar <emilykbonar@gmail.com>
2021-01-08 13:08:16 -0600

    Making newlines work

--- components/Message.js
+++ components/Message.js
@@ -2,9 +2,14 @@ import Link from "next/link";
 
 export default function Message(props) {
 	let time = props.time.toLocaleTimeString() + " " + props.time.toDateString();
+	let text = props.text.split("\n");
 	return (
 		<div className="my-2 ">
-			<div className="p-2 border-2 border-black">{props.text}</div>
+			<div className="p-2 border-2 border-black">
+				{text.map((line) => (
+					<p>{line}</p>
+				))}
+			</div>
 			<div className="flex">
 				<Link href={`./user/${props.name}`}>
 					<a>{props.name}</a>
