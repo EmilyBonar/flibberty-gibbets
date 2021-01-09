ca8c6aad544a51416b638e4e4042c925db1f46f3
emilybonar <emilykbonar@gmail.com>
2021-01-09 08:38:37 -0600

    Add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<script
+					async
+					src="https://www.googletagmanager.com/gtag/js?id=G-13M2K3SVQJ"
+				></script>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+					  
+						gtag('config', 'G-13M2K3SVQJ');`,
+					}}
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
